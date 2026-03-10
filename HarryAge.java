ed52ef372e171d0b38178d7aca0f07a7855e686c
anubhavbal08-rgb <anubhavbal08@gmail.com>
2026-03-10 11:34:56 +0530

    Added W34LEVEL3 Leap Year programs

---
+++ HarryAge.java
@@ -0,0 +1,16 @@
+
+
+public class HarryAge {
+    public static void main(String[] args) {
+
+        // Declaring variables
+        int birthYear = 2000;
+        int currentYear = 2024;
+
+        // Calculating age
+        int age = currentYear - birthYear;
+
+        // Display result
+        System.out.println("Harry's age in 2024 is " + age);
+    }
+}
